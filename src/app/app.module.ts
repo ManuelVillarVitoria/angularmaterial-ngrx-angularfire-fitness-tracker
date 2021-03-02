f43dbbaa9fd3996f514268e56a35e4568cc329ec
Manu <manu.villar.vitoria@gmail.com>
2021-03-03 00:39:50 +0100

    setting up submit button using NgForm and  NgModel directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HammerModule} from '@angular/platform-browser';
 import { FlexLayoutModule } from '@angular/flex-layout';
@@ -32,7 +33,8 @@ import { AppRoutingModule } from './app-routing.module';
     MaterialModule,
     HammerModule,
     AppRoutingModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    FormsModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
