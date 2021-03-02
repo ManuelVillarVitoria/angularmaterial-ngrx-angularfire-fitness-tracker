fb1f74e59762cbd7c6ef1935c1e2a35db8202f57
Manu <manu.villar.vitoria@gmail.com>
2021-03-02 23:18:26 +0100

    adding components structure and routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,33 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HammerModule} from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { MaterialModule } from './material.module'
+import { MaterialModule } from './material.module';
+import { SignupComponent } from './auth/signup/signup.component';
+import { LoginComponent } from './auth/login/login.component';
+import { TrainingComponent } from './training/training.component';
+import { CurrentTrainingComponent } from './training/current-training/current-training.component';
+import { NewTrainingComponent } from './training/new-training/new-training.component';
+import { PastTrainingsComponent } from './training/past-trainings/past-trainings.component';
+import { WelcomeComponent } from './welcome/welcome.component'
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    LoginComponent,
+    TrainingComponent,
+    CurrentTrainingComponent,
+    NewTrainingComponent,
+    PastTrainingsComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MaterialModule,
     HammerModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
