855dc4ee93eff9c700845dbcf2e773d2b5235faf
Manu <manu.villar.vitoria@gmail.com>
2021-03-03 14:23:24 +0100

    Adding a Datepicker and restrict pickable dates to under 18s

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -3,6 +3,8 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from "@angular/material/core";
 
 
 
@@ -11,13 +13,18 @@ import { MatInputModule } from '@angular/material/input';
         MatButtonModule, 
         MatIconModule,
         MatInputModule,
-        MatFormFieldModule  
+        MatFormFieldModule,
+        MatDatepickerModule,
+        MatNativeDateModule
+
     ], 
     exports: [
         MatButtonModule, 
         MatIconModule,
         MatInputModule,
-        MatFormFieldModule 
+        MatFormFieldModule,
+        MatDatepickerModule,
+        MatNativeDateModule
     ]
 })
 export class MaterialModule { }
