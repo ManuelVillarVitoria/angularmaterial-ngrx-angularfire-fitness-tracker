591e30a9fd9e462300cddecd2c1ff0cc024f54a9
Manu <manu.villar.vitoria@gmail.com>
2021-03-03 16:17:30 +0100

    login component with reactive form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule  } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HammerModule} from '@angular/platform-browser';
 import { FlexLayoutModule } from '@angular/flex-layout';
@@ -34,7 +34,8 @@ import { AppRoutingModule } from './app-routing.module';
     HammerModule,
     AppRoutingModule,
     FlexLayoutModule,
-    FormsModule 
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
